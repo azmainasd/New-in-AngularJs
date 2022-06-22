283ab2fb3fddb0d29477c19bbec7c13b265274f9
Azmain <azmainasd@gmail.com>
2022-06-23 00:22:11 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+(function(){
+    // 'use strict'
+    x = 10
+    angular.module('myFirstApp', []) // this empty array[] for dependency
+        .controller('MyFirstController', function(){
+
+        })
+})();
+
